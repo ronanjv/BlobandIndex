814cf36f8dce428a99511b00303d154058111c20
Asher Engelberg <ashercengelberg@icloud.com>
2023-09-17 22:49:57 -0700

    Added Tester Class
    trying some stuff

---
+++ IndexTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class IndexTest {
+}
