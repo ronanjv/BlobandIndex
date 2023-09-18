88f15385662b3d0d0e3cc42c6328f9578c9ed075
Asher Engelberg <ashercengelberg@icloud.com>
2023-09-18 12:49:18 -0700

    Added Back all of the Files
    Added index, blob, Tree, the code config, and the tester. This wasn't done all at once, but it was added from the other repo from where it all works.

---
+++ Index.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.nio.file.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class Index {
+
+    private HashMap<String, String> files = new HashMap<>();
+    private static String path = "objects";
+
+    public Index() throws IOException {
+        init();
+    }
+
+    public static void init() throws IOException {
+        java.nio.file.Path folderPath = Paths.get(path);
+        if (!Files.exists(folderPath)) {
+            Files.createDirectory(folderPath);
+        }
+
+        Path indexPath = folderPath.resolve("index");
+        if (!Files.exists(indexPath)) {
+            Files.createFile(indexPath);
+        }
+    }
+
+    public void addBlob(String fileName) throws IOException, NoSuchAlgorithmException {
+        String hashName = Blob.blob(fileName);
+
+        files.put(fileName, hashName);
+
+        Path indexPath = Paths.get(path + File.separator + "index");
+        try (BufferedWriter writer = Files.newBufferedWriter(indexPath, StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE, StandardOpenOption.APPEND)) {
+            writer.write(fileName + " : " + hashName);
+            writer.newLine();
+        }
+    }
+
+    public void removeBlob(String fileName) throws IOException {
+        files.remove(fileName);
+
+        Path indexPath = Paths.get(path + File.separator + "index");
+        List<String> lines = Files.readAllLines(indexPath);
+        lines.removeIf(line -> line.startsWith(fileName + " : "));
+        Files.write(indexPath, lines);
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Index index = new Index();
+        index.addBlob("test.txt");
+        index.addBlob("test2.txt");
+        index.removeBlob("test.txt");
+    }
+
+    public boolean containsBlob(String fileName) {
+        return files.containsKey(fileName);
+    }
+
+    public String getBlobHash(String fileName) {
+        return files.get(fileName);
+    }
+}
